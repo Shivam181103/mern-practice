8d68664ac6ce03ffd9f9e1822d8315726bccfc4d
Gurpreet <gurpreetsingh2@luminoguru.com>
2023-01-25 15:45:02 +0530

    fsdf

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 require('dotenv').config()
+const cors = require('cors')
 // This is your test secret API key.
 const stripe = require("stripe")('sk_test_51MTfvoSIO9jwyVmqrRp5fXy5CuMjrdavZYvpx8CWyjChluxapXyLhyfm2WkSA3KHpgcQUxiJiHpTN0bws6ZRoMVS00E8dEUa36');
-
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cors())
 
 const calculateOrderAmount = (items) => {
   // Replace this constant with a calculation of the order's amount
